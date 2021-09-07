1cdf301ab0ccc3f5e893bb9993eeb4367af62e3d
azertypow <azertypow@gmail.com>
2021-09-07 14:27:09 +0200

    feat: set header for open api

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+
+header('Access-Control-Allow-Origin: *');
+header ("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // The request is using the POST method
+    header("HTTP/1.1 200 OK");
+    return;
+}
+
+
 require 'kirby/bootstrap.php';
 
 echo (new Kirby)->render();
