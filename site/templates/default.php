28f36ec380cea48b905b42b5eefefa8913fec179
azertypow <azertypow@gmail.com>
2021-09-07 14:26:24 +0200

    chore: proto vuejs with connection

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,27 @@
-<h1><?= $page->title() ?></h1>
+<html>
+<head>
+  <title>API Example</title>
+</head>
+
+<body>
+<script>
+  const csrf = "<?= csrf() ?>";
+
+  fetch("http://localhost:8000/api/pages/projects", {
+    method: "GET",
+    headers: {
+      // "X-CSRF" : csrf
+      'Authorization': 'Basic ' + btoa('public@public.com:publickey'),
+    }
+  })
+    .then(response => response.json())
+    .then(response => {
+      const page = response.data;
+      console.log( page )
+    })
+    .catch(error => {
+      // something went wrong
+    });
+</script>
+</body>
+</html>
