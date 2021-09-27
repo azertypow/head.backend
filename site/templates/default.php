6e3a9b5d56cb8560f9d1a1168e9684aac785b9e0
azertypow <azertypow@gmail.com>
2021-09-27 15:18:06 +0200

    build: v 0.1

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <!--[if IE]><link rel="icon" href="/assets/favicon.ico"><![endif]-->
+    <!--[if IE]><link rel="icon" href="/assets/favicon.png"><![endif]-->
     <title>frontscr</title>
   <link href="/assets/js/0.js" rel="prefetch"><link href="/assets/js/chunk-vendors.js" rel="preload" as="script"><link href="/assets/js/index.js" rel="preload" as="script"><link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icons/favicon-16x16.png"><link rel="manifest" href="/assets/manifest.json"><meta name="theme-color" content="#4DBA87"><meta name="apple-mobile-web-app-capable" content="no"><meta name="apple-mobile-web-app-status-bar-style" content="default"><meta name="apple-mobile-web-app-title" content="frontscr"><link rel="apple-touch-icon" href="/assets/img/icons/apple-touch-icon-152x152.png"><link rel="mask-icon" href="/assets/img/icons/safari-pinned-tab.svg" color="#4DBA87"><meta name="msapplication-TileImage" content="/assets/img/icons/msapplication-icon-144x144.png"><meta name="msapplication-TileColor" content="#000000"></head>
 
