83abb80a309bbf64959aac0db8067e165808e4fe
azertypow <azertypow@gmail.com>
2023-03-20 16:23:50 +0100

    chore: clean code

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -9,16 +9,16 @@ use Kirby\Cms\Page;
 
 require_once('site/phpTools/getImageData.php');
 
-$array = [];
+$array = ['coucou'];
 
 /** @var Page $page */
 foreach ($page->children() as $key => $page) {
 
-  array_push($array, [
+  $array[] = [
     "title" => $page->project_title()->value(),
     "cover" => array_map(fn($image) => getImageData($image), $page->images_covers()->toFiles()->toArray()),
-    "apiUrl"=> $page->url(),
-    "apiUri"   => $page->uri(),
-  ]);
+    "apiUrl" => $page->url(),
+    "apiUri" => $page->uri(),
+  ];
 }
 echo json_encode($array, JSON_UNESCAPED_UNICODE);
